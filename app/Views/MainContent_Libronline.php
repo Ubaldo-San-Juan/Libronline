f6e5f366f7e504b87c15b1163ba0ab8ac0333ffe
unknown <usan9fam@gmail.com>
2020-11-29 17:03:40 -0600

    Adición de vistas

---
+++ app/Views/MainContent_Libronline.php
@@ -0,0 +1,91 @@
+<!--Main content-->
+<div class="mainContent ml-lg-4 col-lg-8 col-sm-12 mt-5 pt-5 align-content-center justify-content-center">
+<article class="post mb-3">
+  <div class="container col-11 pt-2 pb-1">
+    <div class="row">
+      <div class="postHeader w-100">
+        <a href="article.html" class="postTitle col-md-10 col-sm-7 pt-2 d-inline-block">
+          <h1 class="h2">Información acerca de Vaporeon</h1>
+        </a>
+        <p class="postDate text-right col-sm-2 pt-3 float-right d-none d-md-block">
+          2020-11-19
+        </p>
+        <p class="postAuthor">
+          By: Juan Pérez
+        </p>
+      </div>
+      <div class="postBody">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium repellendus doloribus, optio qui aliquam nobis necessitatibus, eligendi atque reiciendis ex ratione, magnam in vero impedit non earum! Quibusdam, suscipit illo!
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor sapiente molestias autem nemo nesciunt nam asperiores enim dolorem a aliquid, suscipit facilis. Suscipit velit aliquid, facere quis beatae eveniet ad.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum provident mollitia autem quaerat facilis, error iure voluptates doloribus commodi recusandae exercitationem dolorum, enim in. Ipsam tempora temporibus recusandae molestias officiis?
+        </p>
+        <div class="imgContainer text-center d-block mb-2">
+          <img src="assets/img/vaporeon.png" alt="Vaporeon">
+        </div>
+      </div>
+      <div class="postMetadata ml-auto mb-2">
+        <p>
+          Categorías:
+        </p>
+        <a href="controlPanel.html">
+          Matemáticas
+        </a>|
+        <a href="#">
+          Cálculo Integral
+        </a>|
+        <a href="#">
+          Álgebra
+        </a>|
+        <a href="#">
+          Geometría
+        </a>
+      </div>
+    </div>
+  </div>
+</article>
+<article class="post mb-3">
+  <div class="container col-11 pt-2 pb-1">
+    <div class="row">
+      <div class="postHeader w-100">
+        <a class="postTitle col-md-10 col-sm-7 pt-2 d-inline-block">
+          <h1 class="h2">Game Grumps - Rueda de la Fortuna</h1>
+        </a>
+        <p class="postDate text-right col-sm-2 pt-3 float-right d-none d-md-block">
+          2020-11-19
+        </p>
+        <p class="postAuthor">
+          By: Víctor Fores
+        </p>
+      </div>
+      <div class="postBody">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium repellendus doloribus, optio qui aliquam nobis necessitatibus, eligendi atque reiciendis ex ratione, magnam in vero impedit non earum! Quibusdam, suscipit illo!
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor sapiente molestias autem nemo nesciunt nam asperiores enim dolorem a aliquid, suscipit facilis. Suscipit velit aliquid, facere quis beatae eveniet ad.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum provident mollitia autem quaerat facilis, error iure voluptates doloribus commodi recusandae exercitationem dolorum, enim in. Ipsam tempora temporibus recusandae molestias officiis?
+        </p>
+        <div class="videoContainer mb-2">
+          <iframe width="802" height="451" src="https://www.youtube.com/embed/FZ5EbNticcY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="postMetadata ml-auto mb-2 text-right">
+        <p>
+          Categorías:
+        </p>
+        <a href="#">
+          Matemáticas
+        </a>|
+        <a href="#">
+          Cálculo Integral
+        </a>|
+        <a href="#">
+          Álgebra
+        </a>|
+        <a href="#">
+          Geometría
+        </a>
+      </div>
+    </div>
+  </div>
+</article>
+</div>
---
+++ app/Views/Siderbar_Libronline.php
@@ -0,0 +1,58 @@
+<!--Sidebar-->
+<aside class="float-right d-lg-block d-none col-lg-3 col-xl-3 mr-lg-5 mt-5 pt-2" id="sidebar">
+  <h2 class="text-center">
+    Categorías
+  </h1>
+  <ul class="list-group shadow mb-3">
+    <li class="list-group-item">
+      <a href="#">
+        Matemáticas
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Lenguaje
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Álgebra
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Cálculo Integral
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Historia
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Programación
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Cultura
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Creatividad
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Vídeo
+      </a>
+    </li>
+    <li class="list-group-item">
+      <a href="#">
+        Bases de Datos
+      </a>
+    </li>
+  </ul>
+</aside>
